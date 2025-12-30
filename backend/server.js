fc27ad5b5100d193178fb960874c8d747a2d0390
Kr-Sahil-Works <sahillearn44@gmail.com>
2025-12-30 17:18:03 +0530

    all completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,6 +13,9 @@ import { app, server } from './socket/socket.js'
 
 
 const PORT = process.env.PORT || 5000 ;
+
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -25,6 +30,13 @@ app.use(cookieParser()); // parse the cookie to our middleware protectRoute
 app.use ("/api/auth", authRoutes)
 app.use ("/api/messages", messageRoutes)
 app.use ("/api/users", userRoutes)
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 server.listen(PORT, ()=>{
     connectToMongoDB();
